049b67f19d4d707a458e671e710b2c49c0b2e4e3
Gaeun Lee <gaeun0800@gmail.com>
2022-07-14 02:35:58 +0900

    데이터를 불러와서 뉴스 데이터 배열을 컴포넌트 배열로 변환

--- React/news-viewer/src/components/NewsList.js
+++ React/news-viewer/src/components/NewsList.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import styled from 'styled-components';
 import NewsItem from './newsItem';
+//import axios from "../../node_modules/axios/index";
+import axios from 'axios';
 
-const NewsListBox = styled.div`
+const NewsListBlock = styled.div`
 box-sizing:border-box;
 padding-bottom:3rem;
 width:768px;
@@ -14,7 +16,7 @@ margin-top:2rem;
     padding-right:1rem;
 }
 `;
-
+/*
 const sampleArticle = {
     title:"제목",
     description:"내용",
@@ -34,5 +36,40 @@ const NewsList = () =>{
         </NewsListBox>
     )
 }
+*/
+const NewsList=()=>{
+    //useState
+    const [articles, setArticles]=useState(null);
+    const [loading,setLoading]=useState(null);
+
+    useEffect(()=>{
+        //async를 사용하는 함수 따로 선언
+        const fetchData= async ()=>{
+            setLoading(true);
+            try{
+                const response= await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=156ada56ab334aa6b35427765dc8a12b',);
+                setArticles(response.data.articles);
+            }catch(e){
+                console.log(e)
+            }
+            setLoading(false);
+        };
+        fetchData();
+    },[]);
+
+    if (loading){
+        return <NewsListBlock>대기 중..</NewsListBlock>;
+    }
+    if (!articles){
+        return null;
+    }
+    return(
+        <NewsListBlock>
+            {articles.map(article=>(<NewsItem key={article.url} article={article
+            }/>))}
+        </NewsListBlock>
+    );
+};
+
 
 export default NewsList;
